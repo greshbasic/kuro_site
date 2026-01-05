00ad5ad5dde4edc81655d413c48cc7be93203186
Gresh Basic <basic.gresham@gmail.com>
2026-01-04 22:20:23 -0600

    updating mailer logic quickly

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -2,12 +2,8 @@ class NotifierMailer < ApplicationMailer
   def new_post(post_name)
     @link = "https://kuro-site.onrender.com/blog/#{post_name}"
 
-    ENV['BLOG_SUBSCRIBERS'].split(",").each do |subscriber|
-      Rails.logger.info "Sending new post email to #{subscriber}"
-      mail(
-        to: subscriber,
-        subject: 'NEW KURO POST',
-      )
-    end
+    subscribers = ENV['BLOG_SUBSCRIBERS'].split(',').map(&:strip)
+
+    mail(bcc: subscribers, subject: 'NEW KURO POST')
   end
 end

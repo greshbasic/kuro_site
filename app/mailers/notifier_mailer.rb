7c592e6d243d2b51cce089e264958618a2fedc1d
Gresh Basic <basic.gresham@gmail.com>
2026-01-04 23:49:34 -0600

    email format changes

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,9 +1,13 @@
 class NotifierMailer < ApplicationMailer
   def new_post(post_name)
     @link = "https://kuro-site.onrender.com/blog/#{post_name}"
+    @title = post_name.split('_', 2)[1]
+    @title = @title[0..-4].tr('_', ' ').titleize
 
     subscribers = ENV['BLOG_SUBSCRIBERS'].split(',').map(&:strip)
+    date = post_name.split('_', 2)[0]
+    subject = "NEW KURO POST (#{date})"
 
-    mail(bcc: subscribers, subject: 'NEW KURO POST')
+    mail(bcc: subscribers, subject:)
   end
 end
